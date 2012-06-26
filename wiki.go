28a29f1f9260430dc8a680bcf5620d165696ad72
Yasutaka Kawamoto <kwmt27@gmail.com>
2012-06-26 21:34:05 +0900

    不要になったコメント部分の削除

--- wiki.go
+++ wiki.go
@@ -34,29 +34,12 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, p)
 }
 
-
-/*
-func main(){
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
-}
-*/
-
 func editHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[lenPath:]
     p, err := loadPage(title)
     if err != nil {
         p = &Page{Title: title}
     }
-/*
-    fmt.Fprintf(w, "<h1>Editing %s</h1>" +
-        "<form action=\"/save/%s\" method=\"POST¥\">" +
-        "<textarea name=\"body\">%s</textarea><br>" +
-        "</form>",
-        p.Title, p.Title, p.Body)
-*/
 	t, _ := template.ParseFiles("edit.html")
 	t.Execute(w, p)
 }
