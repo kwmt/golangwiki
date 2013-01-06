46a9050dffc3ef1c2f9d99f03e43e6fb197d43b3
Yasutaka Kawamoto <kwmt27@gmail.com>
2013-01-06 21:58:27 +0900

    text/template overview確認

---
+++ text_template/overview.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"text/template"
+)
+
+type Inventory struct {
+	Material string
+	Count    uint
+}
+
+func main() {
+
+	sweaters := Inventory{"wool", 17}
+
+	tmpl, err := template.New("test").Parse("{{.Count}} items are made of {{.Material}}")
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(os.Stdout, sweaters)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println()
+}
